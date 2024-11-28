7867f31f38b11b67543d372c4e4ead6633f584fa
Dheeraj Rane <dheerajrane29698@gmail.com>
2024-11-28 18:33:21 +0530

    Add bundles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const App = () => {
-  const path = require("path");
   return <div>Hello World</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
+const path = require("path");
+
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
